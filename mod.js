f53b7fb45ff4ca05913783fa15f36b6d229357ec
Evan Wallace <evan.exe@gmail.com>
2021-10-28 01:32:07 -0400

    publish 0.13.10 to deno

--- mod.js
+++ mod.js
@@ -653,8 +653,8 @@ function createChannel(streamIn) {
     if (isFirstPacket) {
       isFirstPacket = false;
       let binaryVersion = String.fromCharCode(...bytes);
-      if (binaryVersion !== "0.13.9") {
-        throw new Error(`Cannot start service: Host version "${"0.13.9"}" does not match binary version ${JSON.stringify(binaryVersion)}`);
+      if (binaryVersion !== "0.13.10") {
+        throw new Error(`Cannot start service: Host version "${"0.13.10"}" does not match binary version ${JSON.stringify(binaryVersion)}`);
       }
       return;
     }
@@ -1531,7 +1531,7 @@ function convertOutputFiles({ path, contents }) {
 import {
   gunzip
 } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
-var version = "0.13.9";
+var version = "0.13.10";
 var build = (options) => ensureServiceIsRunning().then((service) => service.build(options));
 var serve = (serveOptions, buildOptions) => ensureServiceIsRunning().then((service) => service.serve(serveOptions, buildOptions));
 var transform = (input, options) => ensureServiceIsRunning().then((service) => service.transform(input, options));
