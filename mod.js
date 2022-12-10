3667bedb577025c305d284b7f1c1fa44ee33c93d
Evan Wallace <evan.exe@gmail.com>
2022-12-09 22:51:24 -0500

    publish 0.16.4 to deno

--- mod.js
+++ mod.js
@@ -712,8 +712,8 @@ function createChannel(streamIn) {
     if (isFirstPacket) {
       isFirstPacket = false;
       let binaryVersion = String.fromCharCode(...bytes);
-      if (binaryVersion !== "0.16.3") {
-        throw new Error(`Cannot start service: Host version "${"0.16.3"}" does not match binary version ${quote(binaryVersion)}`);
+      if (binaryVersion !== "0.16.4") {
+        throw new Error(`Cannot start service: Host version "${"0.16.4"}" does not match binary version ${quote(binaryVersion)}`);
       }
       return;
     }
@@ -1666,7 +1666,7 @@ function convertOutputFiles({ path, contents }) {
 
 // lib/deno/mod.ts
 import * as denoflate from "https://deno.land/x/denoflate@1.2.1/mod.ts";
-var version = "0.16.3";
+var version = "0.16.4";
 var build = (options) => ensureServiceIsRunning().then((service) => service.build(options));
 var serve = (serveOptions, buildOptions) => ensureServiceIsRunning().then((service) => service.serve(serveOptions, buildOptions));
 var transform = (input, options) => ensureServiceIsRunning().then((service) => service.transform(input, options));
@@ -1709,7 +1709,7 @@ async function installFromNPM(name, subpath) {
     return finalPath;
   } catch (e) {
   }
-  const url = `https://registry.npmjs.org/${name}/-/${name}-${version}.tgz`;
+  const url = `https://registry.npmjs.org/${name}/-/${name.replace("@esbuild/", "")}-${version}.tgz`;
   const buffer = await fetch(url).then((r) => r.arrayBuffer());
   const executable = extractFileFromTarGzip(new Uint8Array(buffer), subpath);
   await Deno.mkdir(finalDir, {
@@ -1751,7 +1751,7 @@ function getCachePath(name) {
   if (!baseDir)
     throw new Error("Failed to find cache directory");
   const finalDir = baseDir + `/esbuild/bin`;
-  const finalPath = finalDir + `/${name}@${version}`;
+  const finalPath = finalDir + `/${name.replace("/", "-")}@${version}`;
   return { finalPath, finalDir };
 }
 function extractFileFromTarGzip(buffer, file) {
